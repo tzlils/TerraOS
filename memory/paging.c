85327f051adf129cebe667026d6d243c5df2bc73
Tzlil <tzlils@protonmail.com>
2019-09-11 19:51:05 +0300

    two commits in a day? thats gotta be like, a world record or something

--- memory/paging.c
+++ memory/paging.c
@@ -41,3 +41,55 @@ void init_paging()
 	// paging_enable();
 	// printf("Paging was successfully enabled!\n");
 }
+
+/*
+uint32_t PML4[512] __attribute__((aligned(4096)));
+void create_PML4() {
+	int i;
+	for (i = 0; i < 512; i++)
+	{
+		PML4[i] = ((unsigned int)create_PDPT()) | 3;
+	}
+	
+}
+
+uint32_t create_PDPT() {
+	uint32_t PDPT[512] __attribute__((aligned(4096)));
+	int i;
+	for (i = 0; i < 512; i++)
+	{
+		PDPT[i] = ((unsigned int)create_page_directory()) | 3;
+	}
+	return PDPT;
+}
+
+uint32_t create_page_directory() {
+	uint32_t page_directory[512] __attribute__((aligned(4096)));
+	blank_page_directory(page_directory);
+	int i;
+	for (i = 0; i < 512; i++)
+	{
+		page_directory[i] = ((unsigned int)create_page_table()) | 3;
+	}
+	return page_directory;
+}
+
+void blank_page_directory(uint32_t* directory) {
+	int i;
+	for (i = 0; i < 512; i++)
+	{
+		directory[i] = 0x00000002;
+	}
+	
+}
+
+uint32_t create_page_table() {
+	uint32_t page_table[512] __attribute__((aligned(4096)));
+	unsigned int i;
+	for (i = 0; i < 512; i++)
+	{
+		page_table[i] = (i * 0x1000) | 3;
+	}
+	return page_table;
+}
+*/
